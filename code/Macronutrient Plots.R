e79ac748ec4b6473b2fa088893c50e631fc38899
ellalvondohlen <gabriella_vondohlen1@baylor.edu>
2023-06-26 22:22:38 -0400

    updates

--- code/Macronutrient Plots.R
+++ code/Macronutrient Plots.R
@@ -7,8 +7,8 @@ install.packages("qwraps2")
 library(qwraps2)
 install.packages("ggpubr")
 library(ggpubr)
-install.packages('readxl')
-library("readxl")
+install.packages("readxl")
+library(readxl)
 
 #data <- read.csv(file="asa24total.csv", header=T)
 data<-read_xlsx("EDITED VDMT_2022-07-07_81282_Totals (1) (version 1).xlsx")
@@ -18,7 +18,7 @@ excel_sheets("EDITED VDMT_2022-07-07_81282_Totals (1) (version 1).xlsx")
 # only one sheet exists
 
 
-############## CORRECTING THE ID COLUMN ################
+########################## CORRECTING THE ID COLUMN ############################
 data[2]
 names(data)[2] <- 'ID'
 colnames(data)[2]
@@ -27,7 +27,7 @@ num_rows <- length(data$ID)
 
 # to reformat the IDs- from VDMT01 to VDMT001
 for (i in 1:num_rows){
-  new_string <- gsub("^(.{4})(.*)$",         # Apply gsub
+  new_string <- gsub("^(.{4})(.*)$",
                        "\\10\\2",
                        data$ID[i])
   data$ID[i] <- new_string
@@ -49,14 +49,14 @@ names(list_all) <- sheet_names
 
 demograph<-list_all$DEMOGRAPH
 colnames(demograph)
-demo_macro <- demograph[, c("ID", "DOB", "AGE", "SEX", "RACE", 
+demo_macro <- demograph[, c("ID", "DOB", "AGE", "SEX", "RACE",
                            "ETHN", "ORIGIN", "OCCU")]
 
 ################ MACRO OVERVIEW #################################
 
 data_macro <- data[ , c("KCAL", "PROT", "TFAT", "CARB", "VITD")]
 
-#vioin_overview <- ggplot(data_macro, inherit.aes=TRUE) + 
+#vioin_overview <- ggplot(data_macro, inherit.aes=TRUE) +
 #  geom_violin(trim=FALSE)
 
 # maybe i can find something to graph them all?
@@ -108,13 +108,13 @@ text(kcal_hist$mids, kcal_hist$counts, labels = kcal_hist$counts, adj = c(0.5, -
 #KCAL_SEX_df <- table(data$KCAL, data$SEX)
 
 cbind(summary(data$KCAL)) # missing range, standard dev
-# make quantile table, 
-# connect as dataframe both the summary and quantile 
+# make quantile table,
+# connect as dataframe both the summary and quantile
 
-summary_vals_KCAL <- list("min         " = ~min(data$KCAL), 
-                          "1st quartile" = ~quantile(data$KCAL, prob = .25), 
-                          "median      " = ~quantile(data$KCAL, prob = .5), 
-                          "3rd quartile" = ~quantile(data$KCAL, prob = .75), 
+summary_vals_KCAL <- list("min         " = ~min(data$KCAL),
+                          "1st quartile" = ~quantile(data$KCAL, prob = .25),
+                          "median      " = ~quantile(data$KCAL, prob = .5),
+                          "3rd quartile" = ~quantile(data$KCAL, prob = .75),
                           "max         " = ~max(data$KCAL))
 
 summary_KCAL <- summary_table(data, summary_vals_KCAL)
@@ -148,7 +148,7 @@ text(prot_hist$mids, prot_hist$counts, labels = prot_hist$counts, adj = c(0.5, -
 high_prot <- (data$PROT >= 500) == TRUE # row 204 has high prot
 data$PROT[204]
 
-################ 546.634 grams from person 44 - # 204 element # should be 66.66263 
+################ 546.634 grams from person 44 - # 204 element # should be 66.66263
 
 
 par(mar=c(0, 4, 1.1, 2.1))
@@ -206,7 +206,7 @@ data["ID"]
 VDMT_diet<- full_join(demo_macro, data, by="ID")
 colnames(VDMT_diet) # 4 SEX, 17 KCAL, 18 PROT, 19 TFAT, 20 CARB ...
 
-data_macro_sex <- VDMT_diet[ ,c("SEX", "KCAL", "PROT", "TFAT", "CARB", "VITD")]
+data_macro_sex <- VDMT_diet[ ,c("ID", "SEX", "KCAL", "PROT", "TFAT", "CARB", "VITD")]
 data_macro_sex
 
 # CHANGING SEX IDENTIFICATION: 1 to male, 2 to female
@@ -222,64 +222,64 @@ data_macro_sex["SEX"][data_macro_sex["SEX"] == 2] <- "female"
 kcal_sexes <- data_macro_sex[ ,c("SEX", "KCAL")]
 #kcal_sexes[which(kcal_sexes$SEX == 1)]
 ggplot(kcal_sexes, aes(x=KCAL, fill = as.factor(SEX), color = SEX)) +
-  geom_histogram(color = 2, 
-                 alpha = 0.3, 
-                 position = 'identity') + 
-  labs(title = "Kilocalories per Sex", 
-       x = "KiloCalories", 
-       y = "Number of ASA records", 
+  geom_histogram(color = 2,
+                 alpha = 0.3,
+                 position = 'identity') +
+  labs(title = "Kilocalories per Sex",
+       x = "KiloCalories",
+       y = "Number of ASA records",
        fill = "sex")
 
 # PROT
 
 prot_sexes <- data_macro_sex[ , c("SEX", "PROT")]
 ggplot(prot_sexes, aes(x=PROT, fill = as.factor(SEX), color = SEX)) +
-  geom_histogram(color = 3, 
-                 alpha = 0.3, 
-                 position = 'identity') + 
-  labs(title = "Protein per Sex", 
-       x = "Protein (grams)", 
-       y = "Number of ASA records", 
+  geom_histogram(color = 3,
+                 alpha = 0.3,
+                 position = 'identity') +
+  labs(title = "Protein per Sex",
+       x = "Protein (grams)",
+       y = "Number of ASA records",
        fill = "sex")
 
 # TFAT
 
 tfat_sexes <- data_macro_sex[ , c("SEX", "TFAT")]
 ggplot(tfat_sexes, aes(x=TFAT, fill = as.factor(SEX), color = SEX)) +
-  geom_histogram(color = 4, 
-                 alpha = 0.3, 
-                 position = 'identity') + 
-  labs(title = "Fat per Sex", 
-       x = "Fat (grams)", 
-       y = "Number of ASA records", 
+  geom_histogram(color = 4,
+                 alpha = 0.3,
+                 position = 'identity') +
+  labs(title = "Fat per Sex",
+       x = "Fat (grams)",
+       y = "Number of ASA records",
        fill = "sex")
 
 # CARB
 
 carb_sexes <- data_macro_sex[ , c("SEX", "CARB")]
 ggplot(carb_sexes, aes(x = CARB, fill = as.factor(SEX), color = SEX)) +
-  geom_histogram(color = 5, 
-                 alpha = 0.3, 
+  geom_histogram(color = 5,
+                 alpha = 0.3,
                  position = 'identity') +
-  labs(title = "Carbohydrates per Sex", 
-       x = "Carbohydrates (grams)", 
-       y = "Number of ASA records", 
+  labs(title = "Carbohydrates per Sex",
+       x = "Carbohydrates (grams)",
+       y = "Number of ASA records",
        fill = "sex")
 
 # VITD
 
 vitd_sexes <- data_macro_sex[ , c("SEX", "VITD")]
 ggplot(vitd_sexes, aes(x = VITD, fill = as.factor(SEX))) +
-  geom_histogram(color = 6, 
-                 alpha = 0.3, 
-                 position = 'identity') + 
-  labs(title = "Dietary Vitamin D Intake per Sex", 
-       x = "Vitamin D (micrograms)", 
-       y = "Number of ASA records", 
+  geom_histogram(color = 6,
+                 alpha = 0.3,
+                 position = 'identity') +
+  labs(title = "Dietary Vitamin D Intake per Sex",
+       x = "Vitamin D (micrograms)",
+       y = "Number of ASA records",
        fill = "sex")
 
 
-################ PIE GRAPH DEMOGRAPHICS ################
+########################### PIE GRAPH DEMOGRAPHICS #############################
 
 
 par(mfrow=c(1, 1))
@@ -316,7 +316,7 @@ pie(race_slices, labels = race_lbls, main = "Recorded Races")
 
 
 sex_lbls <- c("male", "female")
-pie(c(length(which(demo_macro$SEX == 1)), length(which(demo_macro$SEX == 2))), 
+pie(c(length(which(demo_macro$SEX == 1)), length(which(demo_macro$SEX == 2))),
     labels = sex_lbls, main = "PARTICIPANT SEXES")
 
 
@@ -345,3 +345,36 @@ length(which(demo_macro$ORIGIN == origin_lbls[8])) # 1 USA
 origin_slices <- c(37, 1, 1, 1, 1, 1, 1)
 origin_lbls[1:7]
 pie(origin_slices, labels = origin_lbls[1:7], main = "Recorded Countries of Origin")
+
+
+
+
+######################### VITD vs PLACEBO #############################
+
+supplements <- read_xlsx("Randomization.xlsx")
+colnames(supplements)
+names(supplements)[1] <- 'ID'
+
+colnames(data_macro_sex)
+
+VitD_supp <- full_join(data_macro_sex, supplements, by="ID")
+colnames(VitD_supp)
+
+vitd_supplements <- VitD_supp[ , c('Cohort', 'VITD')]
+ggplot(vitd_supplements, aes(x = VITD, fill = as.factor(Cohort))) +
+  geom_histogram(color = 7,
+                 alpha = 0.3,
+                 position = 'identity') +
+  labs(title = "Dietary Vitamin D Intake per Supplement Group",
+       x = "Vitamin D (micrograms)",
+       y = "Number of ASA records",
+       fill = "Supplement")
+
+
+
+
+
+
+
+
+
